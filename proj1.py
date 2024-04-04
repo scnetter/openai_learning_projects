8139bca5dca478d540ad30b93245ccd2f8310d97
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-04-04 08:16:31 -0500

    Initial openai query and response.

---
+++ proj1.py
@@ -0,0 +1,10 @@
+import os
+import openai 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(model="gpt-3.5-turbo-instruct", 
+                                    prompt="Give me two reasons to learn openai with python", 
+                                    temperature=0, max_tokens=60)
+
+print(response)
