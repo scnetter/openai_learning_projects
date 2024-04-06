0de098f07753b02a478c46e155d267633de8ec8c
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-04-06 11:02:04 -0500

    Create combined prompt

--- NLP_to_SQL/nlp_to_sql.py
+++ NLP_to_SQL/nlp_to_sql.py
@@ -17,3 +17,22 @@ data = df.to_sql(name='Sales', con=temp_db)
 #     result = conn.execute(text("select * from Sales limit 5;"))
 #     # result = conn.execute(text("select sum(SALES) from Sales"))
 
+def create_table_definition(df):
+    prompt = """###sqlite SQL table, with its properties:
+    #
+    # Sales({})
+    #
+    """.format(",".join(str(col) for col in df.columns))
+    return prompt
+
+def prompt_input():
+    nlp_text = input("Enter a request for the data you want: ")
+    return nlp_text
+
+def combine_prompts(df, query_prompt):
+    definition = create_table_definition(df)
+    query_init_string = f"### A query to answer {query_prompt}\nSELECT"
+    return definition+query_init_string
+
+nlp_text = prompt_input()
+combine_prompts(df,nlp_text)
